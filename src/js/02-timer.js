40bc0bbafd7f1cea7680c7eddf27ed14d036dc4c
Yevhen Zakharevych <yzakharevych@fieldcomplete.com>
2024-06-17 17:09:57 +0200

    review fixes

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -80,13 +80,19 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,22 +2,30 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const formEl = document.querySelector('.form');
+const submitButtonEl = document.querySelector('[type="submit"]');
 
 formEl.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(e) {
   e.preventDefault();
 
+  submitButtonEl.disabled = true;
+
   let delay = +formEl.elements.delay.value;
   const step = +formEl.elements.step.value;
   const amount = +formEl.elements.amount.value;
   let position = 1;
+  const duration = delay + step * amount;
 
   while (position !== amount + 1) {
     processPromises(position, delay);
     delay = delay + step;
     position += 1;
   }
+
+  setTimeout(() => {
+    submitButtonEl.disabled = false;
+  }, duration);
 }
 
 function processPromises(position, delay) {
